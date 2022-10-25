e77fe300e479af833159a741bc69d532ee3e8100
Ewa Stempel <ewa.stempel@digital.justice.gov.uk>
2022-10-25 10:44:10 +0100

    Setting the action env variable in the handler test

--- instance-scheduler/main_test.go
+++ instance-scheduler/main_test.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/events"
+	"os"
 	"testing"
 )
 
 func TestHandler(t *testing.T) {
 	t.Run("Testing the lambda function", func(t *testing.T) {
+		os.Setenv("INSTANCE_SCHEDULING_ACTION", "Test")
+
 		_, err := handler(events.APIGatewayProxyRequest{})
 		if err != nil {
 			t.Error("[ERROR]:", err)
