ea3ba15dc33f3983e21e5848c42e9e1efcaf0b60
George Fountopoulos <george.fountopoulos@digital.justice.gov.uk>
2022-10-27 17:15:15 +0100

    - Pass the action as event input - Update README.md and the test - Remove the obsolete env variable INSTANCE_SCHEDULING_ACTION - Remove some unused code (borrowing Ewa's idea)

--- instance-scheduler/main_test.go
+++ instance-scheduler/main_test.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"github.com/aws/aws-lambda-go/events"
-	"net/http"
-	"net/http/httptest"
-	"os"
+	"context"
 	"testing"
 )
 
 func TestHandler(t *testing.T) {
-	os.Setenv("INSTANCE_SCHEDULING_ACTION", "Test")
-
 	t.Run("Successful Request", func(t *testing.T) {
-		ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(200)
-			fmt.Fprintf(w, "Testing")
-		}))
-		defer ts.Close()
-
-		DefaultHTTPGetAddress = ts.URL
-
-		_, err := handler(events.APIGatewayProxyRequest{})
+		_, err := handler(context.TODO(), InstanceSchedulingRequest{Action: "Test"})
 		if err != nil {
 			t.Fatal("Everything should be ok")
 		}
