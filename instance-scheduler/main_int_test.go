73688f4c6a1afa8e70c55058c613539839dd4824
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-09-11 08:11:26 +0100

    🆙 Update arbitrary checked number of skipped instances in integration tests

--- instance-scheduler/main_int_test.go
+++ instance-scheduler/main_int_test.go
@@ -42,7 +42,7 @@ func TestHandler(t *testing.T) {
 		assert.Greater(t, res.ActedUpon, 20, fmt.Sprintf("Number of instances acted upon seems too low. %v", addMsg))
 		assert.Less(t, res.ActedUpon, 100, fmt.Sprintf("Number of instances acted upon seems too high. %v", addMsg))
 		assert.Greater(t, res.Skipped, 0, fmt.Sprintf("Number of skipped instances seems too low. %v", addMsg))
-		assert.Less(t, res.Skipped, 50, fmt.Sprintf("Number of skipped instances seems too high. %v", addMsg))
+		assert.Less(t, res.Skipped, 80, fmt.Sprintf("Number of skipped instances seems too high. %v", addMsg))
 		assert.Greater(t, res.SkippedAutoScaled, 20, fmt.Sprintf("Number of skipped instances that belong to an Auto Scaling group seems too low. %v", addMsg))
 		assert.Less(t, res.SkippedAutoScaled, 100, fmt.Sprintf("Number of skipped instances that belong to an Auto Scaling group seems too high. %v", addMsg))
 	})
