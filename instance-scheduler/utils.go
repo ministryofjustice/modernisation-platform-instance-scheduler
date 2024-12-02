6881a957d020d322e1c54da320ebf7796f16ffd1
mikereiddigital <mike.reid@justice.gov.uk>
2024-12-02 11:11:40 +0000

    fixed assertion error

--- instance-scheduler/utils.go
+++ instance-scheduler/utils.go
@@ -61,10 +61,10 @@ func getNonProductionAccounts(environments string, skipAccountNames string) map[
 	for _, record := range allAccounts {
 		if rec, ok := record.(map[string]interface{}); ok {
 			for key, val := range rec {
-				log.Printf("Checking account: %v", key.(string))
+				log.Printf("Checking account: %v", key)
 				// Skip if the account's name ends with "-production", for example: performance-hub-production will be skipped
 				if !strings.HasSuffix(key, "-production") && (len(skipAccountNames) < 1 || !strings.Contains(skipAccountNames, key)) {
-					log.Printf("Added account list: %v", key.(string))
+					log.Printf("Added account list: %v", key)
 					accounts[key] = val.(string)
 				}
 			}
